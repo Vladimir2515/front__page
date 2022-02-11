9e4118260490e018024dd76b3295c3ed270840c5
Vladimir <vladimir.sitnik29@gmail.com>
2022-02-11 12:26:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Инициализируем Swiper
+const swiper = new Swiper('.departments__slider', {
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	spaceBetween: 20
+});
