7e12cb487c797da55b15e2d38ecb4811aa66e3cd
Vladimir <vladimir.sitnik29@gmail.com>
2022-02-22 12:32:29 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -7,3 +7,30 @@ const swiper = new Swiper('.departments__slider', {
 	},
 	spaceBetween: 20
 });
+
+
+const slider = new Swiper('.clients__swiper', {
+	// Navigation arrows
+
+	spaceBetween: 30,
+	observer: true,
+	observeParents: true,
+	observeSlideChildren: true,
+	slidesPerView: 2.5,
+});
+
+function readMore() {
+	var dots = document.getElementById("dots");
+	var more = document.getElementById("more");
+	var click = document.getElementById("click");
+
+	if (dots.style.display === "none") {
+		dots.style.display = "inline";
+		click.innerHTML = "Read more";
+		more.style.display = "none";
+	} else {
+		dots.style.display = "none";
+		click.innerHTML = "Hide";
+		more.style.display = "inline";
+	}
+}
